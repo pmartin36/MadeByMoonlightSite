ce8692ee1ffdd8a90266ea543b642db36baa880b
pmartin36 <pmartin36@gmail.com>
2020-05-24 17:45:58 -0400

    update deps
    update version
    
    update again
    
    update deps

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener, ViewChild, ElementRef, Renderer } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { EasingLogic } from 'ng2-page-scroll';
 
@@ -16,8 +16,18 @@ export class NavComponent implements OnInit {
   justScrolled: boolean;
   timer: any;
 
-  @ViewChild('scroller')
+  @ViewChild('scroller', { static: true })
   scroller: ElementRef;
+
+  myEasing: EasingLogic = {
+    ease: (t: number, b: number, c: number, d: number): number => {
+        // easeInOutExpo easing
+        if (t === 0) { return b; }
+        if (t === d) { return b + c; }
+        if ((t /= d / 2) < 1) { return c / 2 * Math.pow(2, 10 * (t - 1)) + b; }
+        return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+    }
+  };
 
   @HostListener('window:scroll', ['$event'])
   scrollHandle(event) {
@@ -30,28 +40,18 @@ export class NavComponent implements OnInit {
     },250)
   }
 
-  determinePageLocation(){
+  determinePageLocation() {
     const body = document.body;
     const html = document.documentElement;
     const height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
     this.top = 131 * window.scrollY / height  + '%';
 
     const scrollLocation = Math.round(window.scrollY / html.clientHeight);
-    const index = Math.min( scrollLocation, this.sections.length-1);
-    this.focusedSection = this.sections[index];  
+    const index = Math.min( scrollLocation, this.sections.length - 1);
+    this.focusedSection = this.sections[index];
   }
 
-  myEasing: EasingLogic = {
-    ease: (t: number, b: number, c: number, d: number): number => {
-        // easeInOutExpo easing
-        if (t === 0) return b;
-        if (t === d) return b + c;
-        if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
-        return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
-    }
-  };
-
-  constructor(private renderer: Renderer) { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
     this.determinePageLocation();
